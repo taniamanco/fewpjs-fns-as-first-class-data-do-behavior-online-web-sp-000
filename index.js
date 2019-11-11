c3f24601857759b993e0217c7a07bebae172e429
Tania Manco <belalov.tania16@gmail.com>
2019-11-11 18:52:20 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
+
+function greet(time) {
+  const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+} 
